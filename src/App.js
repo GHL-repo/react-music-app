653e4a0b8f0f2988b7357bc0ce51ec2a92cd36bb
guus <ghligtvoet@gmail.com>
2022-02-06 21:15:45 +0100

    app data added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./styles/app.scss";
 import Player from "./components/Player.js"
 import Song from "./components/Song.js"
+import data from "./data.js"
 
 
 function App() {
