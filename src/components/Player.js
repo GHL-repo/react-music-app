cb8b0b96d120e0d54b4bcdd387e88de0f0f6912e
guus <ghligtvoet@gmail.com>
2022-02-06 23:28:01 +0100

    update time

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,10 +1,10 @@
-import React, {useRef} from "react";
+import React, {useState, useRef} from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlay } from "@fortawesome/free-solid-svg-icons";
 import { faStepBackward } from "@fortawesome/free-solid-svg-icons";
 import { faStepForward } from "@fortawesome/free-solid-svg-icons";
 
-const Player = ( {currentSong, isPlaying, setIsPlaying} ) => {
+const Player = ({currentSong, isPlaying, setIsPlaying}) => {
     //Ref
     const audioRef = useRef(null);
     //Event Handlers
@@ -15,23 +15,46 @@ const Player = ( {currentSong, isPlaying, setIsPlaying} ) => {
         } else {
             audioRef.current.play(); 
             setIsPlaying(true);
-        }
+        };
     };
+    const timeUpdateHandler = (e) => {
+        const current = e.target.currentTime;
+        const duration = e.target.duration;
+        setSongInfo({...songInfo, currentTime: current, duration});
+    };
+    const getTime = (time) => {
+        //Return time in human readable form 
+        return(
+            Math.floor(time / 60) + ":" + ("0" + Math.floor(time % 60)).slice(-2)
+        );
+    };
+    //State
+    const [songInfo, setSongInfo] = useState ({
+        currentTime: null,
+        duration: null,
+
+    });
+ 
     return (
       <div className="player">
         <div className="time-control">
-            <p>Start Time</p>
+            <p>{getTime(songInfo.currentTime)}</p>
             <input type="range" />
-            <p>End Time</p>
+            <p>{getTime(songInfo.duration)}</p>
         </div>
         <div class="play-control">
             <FontAwesomeIcon className="previous" icon={faStepBackward} size="3x"/>
             <FontAwesomeIcon onClick={playSongHandler} className="play" icon={faPlay} size="3x" />
             <FontAwesomeIcon className="next" icon={faStepForward} size="3x" />
         </div>
-        <audio ref={audioRef} src={currentSong.audio}></audio>
+        <audio 
+            onLoadedMetadata={timeUpdateHandler}
+            onTimeUpdate={timeUpdateHandler}
+            ref={audioRef} 
+            src={currentSong.audio}>
+        </audio>
       </div>  
-    )
-}
+    );
+};
 
 export default Player;
